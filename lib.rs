057dcee16af6bf3d69a0419de8e0c6a38b3b9226
Sniezka <dygons14@gmail.com>
2023-11-28 18:14:04 +0100

    Update

--- lib.rs
+++ lib.rs
@@ -1,13 +1,26 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
+extern crate alloc;
+
+#[cfg(test)]
+mod tests;
+
+#[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum MyErrors {
+    CustomError,
+}
 
 #[ink::contract]
 mod flipper {
+    use crate::MyErrors;
+    // use std::result::Result;
 
     #[ink(storage)]
     #[derive(Default)]
     pub struct Flipper {
         last_timestamp: u128,
         value: u128,
+        v: u32,
     }
 
     impl Flipper {
@@ -16,6 +29,7 @@ mod flipper {
             Self {
                 last_timestamp: Self::env().block_timestamp() as u128,
                 value: u128::default(),
+                v: u32::default(),
             }
         }
 
@@ -24,6 +38,7 @@ mod flipper {
             Self {
                 last_timestamp: Self::env().block_timestamp() as u128,
                 value: u128::default(),
+                v: u32::default(),
             }
         }
 
@@ -32,8 +47,12 @@ mod flipper {
             let current_timestamp = self.env().block_timestamp().clone() as u128;
             let timestamp_delta = (current_timestamp - self.last_timestamp) as u128;
 
-            let multiplication =1000000000000000000000000000000u128.checked_mul(timestamp_delta).unwrap_or_else(|| panic!("Multiplication overflow"));
-            let division = multiplication.checked_div(1000000000000).unwrap_or_else(|| panic!("Division overflow"));
+            let multiplication = 1000000000000000000000000000000u128
+                .checked_mul(timestamp_delta)
+                .unwrap_or_else(|| panic!("Multiplication overflow"));
+            let division = multiplication
+                .checked_div(1000000000000)
+                .unwrap_or_else(|| panic!("Division overflow"));
 
             let value = division;
             self.last_timestamp = current_timestamp as u128;
@@ -43,6 +62,28 @@ mod flipper {
         #[ink(message)]
         pub fn get_timestamps(&self) -> (u128, u128) {
             (self.last_timestamp, self.value)
+        }
+        #[ink(message)]
+        pub fn get(&self) -> u32 {
+            self.v
+        }
+        #[ink(message)]
+        pub fn set(&mut self, v: u32) {
+            self.v = v;
+        }
+        #[ink(message)]
+        pub fn get_and_print(&self) -> u32 {
+            let caller = self.env().caller();
+            ink::env::debug_message(ink::prelude::format!("got a call from {:?}", caller).as_str());
+            ink::env::debug_print!("Print");
+            ink::env::debug_println!("Println");
+
+            self.v
+        }
+
+        #[ink(message)]
+        pub fn err_return(&self) -> Result<u32, MyErrors> {
+            return Err(MyErrors::CustomError);
         }
     }
 
@@ -74,15 +115,15 @@ mod flipper {
 
             let mut last_timestamp = timestamps.0;
 
-            for _ in 1..1000 {
+            for _ in 1..5 {
                 let timestamps: (u128, u128) = session.call("get_timestamps", NO_ARGS, None)??;
 
-                println!(
-                    "timestamp diff = {:?}  [{:?} - {:?}]",
-                    timestamps.0 - last_timestamp,
-                    timestamps.0,
-                    last_timestamp
-                );
+                // println!(
+                //     "timestamp diff = {:?}  [{:?} - {:?}]",
+                //     timestamps.0 - last_timestamp,
+                //     timestamps.0,
+                //     last_timestamp
+                // );
 
                 last_timestamp = timestamps.0;
 
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
 cargo test -- --show-output
+
+### prequesites:
+
+rustup toolchain install nightly
+rustup component add rust-src --toolchain nightly-x86_64
+rustup target add wasm32-unknown-unknown
+
+substrate-contracts-node -lerror,runtime::contracts=debug
+
+cargo +nightly test -- --nocapture
