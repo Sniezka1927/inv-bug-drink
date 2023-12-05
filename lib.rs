d5cbf77e6b2d202b3458f96d483ccd7b25f726fd
Sniezka <dygons14@gmail.com>
2023-12-05 14:07:58 +0100

    Added other possible entrypoints and tests

--- lib.rs
+++ lib.rs
@@ -1,26 +1,24 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 extern crate alloc;
 
-#[cfg(test)]
-mod tests;
-
-#[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
-#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
-pub enum MyErrors {
-    CustomError,
-}
-
 #[ink::contract]
 mod flipper {
-    use crate::MyErrors;
+
     // use std::result::Result;
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum TestError {
+        MULOverflow,
+        DIVOverflow,
+        ADDOverflow,
+        SUBOverflow,
+    }
 
     #[ink(storage)]
     #[derive(Default)]
     pub struct Flipper {
         last_timestamp: u128,
         value: u128,
-        v: u32,
     }
 
     impl Flipper {
@@ -29,7 +27,6 @@ mod flipper {
             Self {
                 last_timestamp: Self::env().block_timestamp() as u128,
                 value: u128::default(),
-                v: u32::default(),
             }
         }
 
@@ -38,8 +35,70 @@ mod flipper {
             Self {
                 last_timestamp: Self::env().block_timestamp() as u128,
                 value: u128::default(),
-                v: u32::default(),
             }
+        }
+
+        #[ink(message)]
+        pub fn update_timestamp_diffrent_operations_mul(&mut self) -> Result<(), TestError> {
+            let current_timestamp = self.env().block_timestamp().clone() as u128;
+            let timestamp_delta = (current_timestamp - self.last_timestamp) as u128;
+
+            let multiplication =
+                match 1000000000000000000000000000000u128.checked_mul(timestamp_delta) {
+                    Some(result) => result,
+                    None => return Err(TestError::MULOverflow),
+                };
+
+            let value = match multiplication.checked_mul(1000000000000) {
+                Some(result) => result,
+                None => return Err(TestError::MULOverflow),
+            };
+
+            // To don't let compiler ignore the value
+            ink::env::debug_println!("value = {}", value);
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn update_timestamp_diffrent_operations_add(&mut self) -> Result<(), TestError> {
+            let current_timestamp = self.env().block_timestamp().clone() as u128;
+            let timestamp_delta = (current_timestamp - self.last_timestamp) as u128;
+
+            let multiplication =
+                match 1000000000000000000000000000000u128.checked_mul(timestamp_delta) {
+                    Some(result) => result,
+                    None => return Err(TestError::MULOverflow),
+                };
+
+            let value = match multiplication.checked_add(1000000000000) {
+                Some(result) => result,
+                None => return Err(TestError::ADDOverflow),
+            };
+
+            // To don't let compiler ignore the value
+            ink::env::debug_println!("value = {}", value);
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn update_timestamp_without_store(&mut self) -> Result<(), TestError> {
+            let current_timestamp = self.env().block_timestamp().clone() as u128;
+            let timestamp_delta = (current_timestamp - self.last_timestamp) as u128;
+
+            let multiplication =
+                match 1000000000000000000000000000000u128.checked_mul(timestamp_delta) {
+                    Some(result) => result,
+                    None => return Err(TestError::MULOverflow),
+                };
+
+            let value = match multiplication.checked_div(1000000000000) {
+                Some(result) => result,
+                None => return Err(TestError::DIVOverflow),
+            };
+
+            // To don't let compiler ignore the division
+            ink::env::debug_println!("division = {}", value);
+            Ok(())
         }
 
         #[ink(message)]
@@ -47,12 +106,9 @@ mod flipper {
             let current_timestamp = self.env().block_timestamp().clone() as u128;
             let timestamp_delta = (current_timestamp - self.last_timestamp) as u128;
 
-            let multiplication = 1000000000000000000000000000000u128
-                .checked_mul(timestamp_delta)
-                .unwrap_or_else(|| panic!("Multiplication overflow"));
-            let division = multiplication
-                .checked_div(1000000000000)
-                .unwrap_or_else(|| panic!("Division overflow"));
+            // 10^30 * delta
+            let multiplication = 1000000000000000000000000000000u128 * (timestamp_delta);
+            let division = multiplication / (1000000000000);
 
             let value = division;
             self.last_timestamp = current_timestamp as u128;
@@ -62,28 +118,6 @@ mod flipper {
         #[ink(message)]
         pub fn get_timestamps(&self) -> (u128, u128) {
             (self.last_timestamp, self.value)
-        }
-        #[ink(message)]
-        pub fn get(&self) -> u32 {
-            self.v
-        }
-        #[ink(message)]
-        pub fn set(&mut self, v: u32) {
-            self.v = v;
-        }
-        #[ink(message)]
-        pub fn get_and_print(&self) -> u32 {
-            let caller = self.env().caller();
-            ink::env::debug_message(ink::prelude::format!("got a call from {:?}", caller).as_str());
-            ink::env::debug_print!("Print");
-            ink::env::debug_println!("Println");
-
-            self.v
-        }
-
-        #[ink(message)]
-        pub fn err_return(&self) -> Result<u32, MyErrors> {
-            return Err(MyErrors::CustomError);
         }
     }
 
@@ -100,7 +134,7 @@ mod flipper {
         enum BundleProvider {}
 
         #[drink::test]
-        fn bugged_value() -> Result<(), Box<dyn Error>> {
+        fn test_update_timestamp() -> Result<(), Box<dyn Error>> {
             let contract = BundleProvider::Flipper.bundle()?;
 
             let mut session = Session::<MinimalRuntime>::new()?.deploy_bundle_and(
@@ -115,19 +149,77 @@ mod flipper {
 
             let mut last_timestamp = timestamps.0;
 
-            for _ in 1..5 {
+            for _ in 1..200 {
                 let timestamps: (u128, u128) = session.call("get_timestamps", NO_ARGS, None)??;
 
-                // println!(
-                //     "timestamp diff = {:?}  [{:?} - {:?}]",
-                //     timestamps.0 - last_timestamp,
-                //     timestamps.0,
-                //     last_timestamp
-                // );
+                // Timestamp diff appears to be always 0
+                println!(
+                    "timestamp diff = {:?}  [{:?} - {:?}]",
+                    timestamps.0 - last_timestamp,
+                    timestamps.0,
+                    last_timestamp
+                );
 
                 last_timestamp = timestamps.0;
 
                 session.call("update_timestamp", NO_ARGS, None)??;
+            }
+
+            Ok(())
+        }
+
+        #[drink::test]
+        fn test_update_timestamp_without_updating_state() -> Result<(), Box<dyn Error>> {
+            let contract = BundleProvider::Flipper.bundle()?;
+
+            let mut session = Session::<MinimalRuntime>::new()?.deploy_bundle_and(
+                contract,
+                "new",
+                NO_ARGS,
+                vec![],
+                None,
+            )?;
+
+            for _ in 1..200 {
+                session.call("update_timestamp_without_store", NO_ARGS, None)??;
+            }
+
+            Ok(())
+        }
+
+        #[drink::test]
+        fn test_update_timestamp_add_operation() -> Result<(), Box<dyn Error>> {
+            let contract = BundleProvider::Flipper.bundle()?;
+
+            let mut session = Session::<MinimalRuntime>::new()?.deploy_bundle_and(
+                contract,
+                "new",
+                NO_ARGS,
+                vec![],
+                None,
+            )?;
+
+            for _ in 1..200 {
+                session.call("update_timestamp_diffrent_operations_add", NO_ARGS, None)??;
+            }
+
+            Ok(())
+        }
+
+        #[drink::test]
+        fn test_update_timestamp_mul_operation() -> Result<(), Box<dyn Error>> {
+            let contract = BundleProvider::Flipper.bundle()?;
+
+            let mut session = Session::<MinimalRuntime>::new()?.deploy_bundle_and(
+                contract,
+                "new",
+                NO_ARGS,
+                vec![],
+                None,
+            )?;
+
+            for _ in 1..200 {
+                session.call("update_timestamp_diffrent_operations_mul", NO_ARGS, None)??;
             }
 
             Ok(())
